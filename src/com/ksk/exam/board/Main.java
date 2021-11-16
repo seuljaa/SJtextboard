283c9338ad3d56398c259849c9939e66b9374a53
seuljaa <seuljaa940503@gmail.com>
2021-11-16 12:46:40 +0900

    입력

--- src/com/ksk/exam/board/Main.java
+++ src/com/ksk/exam/board/Main.java
@@ -1,12 +1,20 @@
 package com.ksk.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
     // write your code here
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.println("명령)");
+    System.out.print("명령)");
+    Scanner sc = new Scanner(System.in);
+    String a;
+    a = sc.next();
+    System.out.println("입력된 명령어 : " + a);
+
+
     System.out.println("== 프로그램 종료 ==");
   }
 }
