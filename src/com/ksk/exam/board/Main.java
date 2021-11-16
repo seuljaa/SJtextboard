ba7629d03f93a988236ec2537245e16f9d96c412
seuljaa <seuljaa940503@gmail.com>
2021-11-16 13:45:06 +0900

    exit까지반복

--- src/com/ksk/exam/board/Main.java
+++ src/com/ksk/exam/board/Main.java
@@ -6,13 +6,18 @@ public class Main {
 
   public static void main(String[] args) {
     // write your code here
+    Scanner sc = new Scanner(System.in);
+
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.print("명령)");
-    Scanner sc = new Scanner(System.in);
-    String a = sc.nextLine();
-    System.out.println("입력된 명령어 : " + a);
+    while(true)
+    {
+      System.out.print("명령)");
+      String text = sc.nextLine();
+      System.out.println("입력된 명령어는 : " + text );
+      if(text.equals("exit"))
+      {break;}
+    }
     System.out.println("== 프로그램 종료 ==");
-    sc.close();
   }
 }
