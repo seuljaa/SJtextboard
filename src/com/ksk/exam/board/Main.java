c11dda7d96aa729e09f22063fdb50450debab71a
seuljaa <seuljaa940503@gmail.com>
2021-11-16 12:51:21 +0900

    입력수정

--- src/com/ksk/exam/board/Main.java
+++ src/com/ksk/exam/board/Main.java
@@ -10,11 +10,9 @@ public class Main {
     System.out.println("== 프로그램 시작 ==");
     System.out.print("명령)");
     Scanner sc = new Scanner(System.in);
-    String a;
-    a = sc.next();
+    String a = sc.nextLine();
     System.out.println("입력된 명령어 : " + a);
-
-
     System.out.println("== 프로그램 종료 ==");
+    sc.close();
   }
 }
