1358c9555ccacc923fe7513c77cba5b6bbf1133e
seuljaa <seuljaa940503@gmail.com>
2021-11-17 13:10:13 +0900

    수정

--- src/com/ksk/exam/board/Main.java
+++ src/com/ksk/exam/board/Main.java
@@ -7,7 +7,7 @@ public class Main {
   public static void main(String[] args) {
     // write your code here
     Scanner sc = new Scanner(System.in);
-    int id = 1;
+    int idLast = 0;
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
     while(true)
@@ -17,21 +17,20 @@ public class Main {
       System.out.println("입력된 명령어는 : " + text );
       if(text.equals("exit"))
       {break;}
-      else if(text.equals("/usr/article/write"))
-      {
-
+      else if(text.equals("/usr/article/write")) {
         System.out.println("- 게시물 등록 -");
         System.out.print("제목 :");
         String title = sc.nextLine();
         System.out.print("내용 :");
         String body = sc.nextLine();
-
+        int id = idLast + 1;
+        idLast = id;
         Article article = new Article(id, title, body);
         System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
-        id++;
       }
     }
+
     System.out.println("== 프로그램 종료 ==");
   }
 }
@@ -46,9 +45,10 @@ class Article {
    this.title = title;
    this.body = body;
   }
+
   public String toString()
   {
-    return String.format("게시물 개수 : %d, 제목 : %s, 내용 : %s", id, title, body);
+    return String.format("{게시물 개수 : %d, 제목 : %s, 내용 : %s}", id, title, body);
   }
 
 }
