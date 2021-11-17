667f0f8067baecbafa318c3fe2b36bba0c4b67d3
seuljaa <seuljaa940503@gmail.com>
2021-11-17 10:45:04 +0900

    article클래스와객체생성

--- src/com/ksk/exam/board/Main.java
+++ src/com/ksk/exam/board/Main.java
@@ -7,7 +7,7 @@ public class Main {
   public static void main(String[] args) {
     // write your code here
     Scanner sc = new Scanner(System.in);
-    int i = 1;
+    int id = 1;
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
     while(true)
@@ -19,15 +19,28 @@ public class Main {
       {break;}
       else if(text.equals("/usr/article/write"))
       {
+
         System.out.println("- 게시물 등록 -");
         System.out.print("제목 :");
         String title = sc.nextLine();
         System.out.print("내용 :");
         String body = sc.nextLine();
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", i);
-        i++;
+
+        Article article = new Article();
+        article.id = id;
+        article.title = title;
+        article.body = body;
+
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+        id++;
       }
     }
     System.out.println("== 프로그램 종료 ==");
   }
+}
+class Article {
+  int id;
+  String title;
+  String body;
 }
