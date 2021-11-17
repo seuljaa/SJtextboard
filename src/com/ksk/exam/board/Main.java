69e7eff40aa4d13c73dd5cd483fa6710e5f534fb
seuljaa <seuljaa940503@gmail.com>
2021-11-17 10:17:54 +0900

    게시물등록시마다번호증가

--- src/com/ksk/exam/board/Main.java
+++ src/com/ksk/exam/board/Main.java
@@ -7,7 +7,7 @@ public class Main {
   public static void main(String[] args) {
     // write your code here
     Scanner sc = new Scanner(System.in);
-
+    int i = 1;
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
     while(true)
@@ -17,6 +17,16 @@ public class Main {
       System.out.println("입력된 명령어는 : " + text );
       if(text.equals("exit"))
       {break;}
+      else if(text.equals("/usr/article/write"))
+      {
+        System.out.println("- 게시물 등록 -");
+        System.out.print("제목 :");
+        String title = sc.nextLine();
+        System.out.print("내용 :");
+        String body = sc.nextLine();
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", i);
+        i++;
+      }
     }
     System.out.println("== 프로그램 종료 ==");
   }
